d00e5df8a17849ecd1e90152a295d059d3419230
Sandeep1816 <sandeepavala2@gmail.com>
2025-06-27 10:41:41 +0530

    run time changed3

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
+// // next.config.mjs
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   // Add other settings below as needed
+// }
+
+// export default nextConfig
+
+
 // next.config.mjs
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Add other settings below as needed
+  experimental: {
+    serverComponentsExternalPackages: ["googleapis", "google-auth-library"],
+  },
 }
 
 export default nextConfig
